01a007944be5c43036dfc3adc5e3fa5c9d14b7af
naiara.medeiros <naiara.medeiros@wildlifestudios.com>
2026-05-11 15:39:22 -0300

    feat: adiciona get model response

---
+++ app/services/llm_service.py
@@ -0,0 +1,12 @@
+from litellm import completion
+
+
+async def get_model_response(prompt: str, provider: str, model_id: str):
+    try:
+        response = completion(
+            model=f"{provider}/{model_id}",
+            messages=[{"content": prompt, "role": "user"}],
+        )
+        return response.choices[0].message.content
+    except Exception as e:
+        raise Exception(f"Erro ao chamar o modelo {model_id}: {str(e)}")
