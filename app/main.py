310467ee67c55a1a9cf36bcfdc948a567e4868e6
Naiara Medeiros <naiara.medeiros.costa.almeida@ccc.ufcg.edu.br>
2026-05-12 19:44:47 -0300

    chore: adiciona rota para dataset

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 from contextlib import asynccontextmanager
 from fastapi import FastAPI
 from app.core.database import create_db_and_tables
+from app.api.routes.dataset_routes import router as dataset_router
 
 
 @asynccontextmanager
@@ -13,6 +14,8 @@ async def lifespan(app: FastAPI):
 
 
 app = FastAPI(lifespan=lifespan, title="Minha API com SQLModel")
+
+app.include_router(dataset_router, prefix="/datasets", tags=["Datasets"])
 
 
 @app.get("/")
