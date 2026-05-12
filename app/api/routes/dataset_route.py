53091b11f168da99f8a8b42f79e1c60b4c7efcce
Naiara Medeiros <naiara.medeiros.costa.almeida@ccc.ufcg.edu.br>
2026-05-12 19:10:11 -0300

    chore: cria service para o dataset

---
+++ app/api/routes/dataset_route.py
@@ -0,0 +1,14 @@
+from fastapi import APIRouter, Depends, UploadFile
+from sqlmodel import Session
+from app.core.database import get_session
+from app.services.dataset_service import process_dataset_upload
+
+
+router = APIRouter()
+
+
+@router.post("/upload-dataset")
+async def upload_dataset(file: UploadFile, db: Session = Depends(get_session)):
+    content = await file.read()
+    dataset = await process_dataset_upload(content, file.filename, db)
+    return {"message": "Dataset criado com sucesso", "id": dataset.id}
--- app/services/dataset_service.py
+++ app/services/dataset_service.py
@@ -1,6 +1,52 @@
-async def create_csv_dataset():
-    pass
+import pandas as pd
+from io import BytesIO
+from pandas import DataFrame
+from sqlmodel import Session
+from app.models.dataset import Dataset, TestCase
 
 
-async def create_json_dataset():
-    pass
+async def process_dataset_upload(file_content: bytes, file_name: str, db: Session):
+    try:
+        buffer = BytesIO(file_content)
+        df, format_name = get_dataframe(buffer, file_name)
+        dataset = create_dataset(file_name, format_name, len(df), db)
+        create_test_case(df, dataset, db)
+
+        return dataset
+    except Exception as e:
+        raise ValueError("Erro ao processar dataset:", e)
+
+
+def create_dataset(name: str, format_name: str, number_lines: int, db: Session):
+
+    dataset = Dataset(name=name, format_name=format_name, number_lines=number_lines)
+
+    db.add(dataset)
+    db.commit()
+    db.refresh(dataset)
+
+    return dataset
+
+
+def create_test_case(df: DataFrame, dataset: Dataset, db: Session):
+    for _, row in df.iterrows():
+        test_case = TestCase(
+            query=row["query"],
+            expected_answer=row["expected_answer"],
+            dataset_id=dataset.id,
+        )
+
+        db.add(test_case)
+    db.commit()
+
+
+def get_dataframe(biffer: BytesIO, file_name: str):
+    format_name = file_name.split(".")[-1].lower()
+
+    if format_name == "csv":
+        df = pd.read_csv(biffer)
+    elif format_name == "json":
+        df = pd.read_json(biffer)
+    else:
+        raise ValueError("Formato não suportado")
+    return df, format_name
