c8defeeef48951f57b4b5cbc68cd85bccc95419a
Naiara Medeiros <naiara.medeiros.costa.almeida@ccc.ufcg.edu.br>
2026-05-12 22:23:43 -0300

    chore: cria llm service

--- app/services/llm_service.py
+++ app/services/llm_service.py
@@ -1,12 +1,90 @@
-from litellm import completion
+from litellm import acompletion
+from sqlmodel import Session, select
+from app.models.model import Model
+from app.models.dataset import TestCase
+from app.models.execution import ExecutionResult
 
 
-async def get_model_response(prompt: str, provider: str, model_id: str):
-    try:
-        response = completion(
-            model=f"{provider}/{model_id}",
-            messages=[{"content": prompt, "role": "user"}],
+async def run_experiment(
+    dataset_id: int,
+    provider: str,
+    model_id: str,
+    temperature: float,
+    prompt: str,
+    db: Session,
+):
+    model_config = create_model(model_id, provider, temperature, prompt, db)
+
+    statement = select(TestCase).where(TestCase.dataset_id == dataset_id)
+    test_cases = db.exec(statement).all()
+
+    results = []
+
+    for test in test_cases:
+        prompt = f"System: {prompt}\nUser: {test.query}"
+        response_data = await get_model_response(
+            prompt, provider, model_id, temperature
         )
-        return response
-    except Exception as e:
-        raise Exception(f"Erro ao chamar o modelo {model_id}: {str(e)}")
+
+        result = create_execution_result(
+            model_config.id,
+            response_data.choices[0].message.content,
+            test.id,
+            response_data.usage.prompt_tokens,
+            response_data.usage.completion_tokens,
+            (response_data.usage.prompt_tokens + response_data.usage.completion_tokens),
+            db,
+        )
+        results.append(result)
+
+    db.commit()
+    print(results)
+    return results
+
+
+async def get_model_response(
+    prompt: str, provider: str, model_id: str, temperature: float
+):
+    return await acompletion(
+        model=f"{provider}/{model_id}",
+        messages=[{"content": prompt, "role": "user"}],
+        temperature=temperature,
+    )
+
+
+def create_model(
+    name: str, provider: str, temperature: float, prompt: str, db: Session
+):
+    model = Model(
+        name=name,
+        provider=provider,
+        temperature=temperature,
+        prompt=prompt,
+    )
+
+    db.add(model)
+    db.commit()
+    db.refresh(model)
+
+    return model
+
+
+def create_execution_result(
+    model_id: int,
+    model_response: str,
+    testcase_id: int,
+    prompt_tokens: int,
+    completion_tokens: int,
+    total_tokens: int,
+    db: Session,
+):
+    result = ExecutionResult(
+        model_id=model_id,
+        testcase_id=testcase_id,
+        model_response=model_response,
+        prompt_tokens=prompt_tokens,
+        completion_tokens=completion_tokens,
+        total_tokens=total_tokens,
+    )
+    db.add(result)
+    return result
