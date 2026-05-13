c7697dbf64180a72e37d5c67089162d7e97c0040
Naiara Medeiros <naiara.medeiros.costa.almeida@ccc.ufcg.edu.br>
2026-05-12 22:25:24 -0300

    chore: adiciona rota no main

--- app/main.py
+++ app/main.py
@@ -1,7 +1,11 @@
 from contextlib import asynccontextmanager
 from fastapi import FastAPI
+import os
 from app.core.database import create_db_and_tables
 from app.api.routes.dataset_routes import router as dataset_router
+from app.api.routes.llm_routes import router as llm_router
+
+os.environ["GEMINI_API_KEY"] = os.getenv("GEMINI_API_KEY")
 
 
 @asynccontextmanager
@@ -15,7 +19,9 @@ async def lifespan(app: FastAPI):
 
 app = FastAPI(lifespan=lifespan, title="Minha API com SQLModel")
 
-app.include_router(dataset_router, prefix="/datasets", tags=["Datasets"])
+app.include_router(dataset_router, prefix="/api", tags=["Datasets"])
+
+app.include_router(llm_router, prefix="/api", tags=["LLM"])
 
 
 @app.get("/")
